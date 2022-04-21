d40e432d22cf5c68f7ada35135193284d7a6076c
Calvin John <calvinjohn@Calvins-MacBook-Pro.local>
2022-04-20 17:37:23 -0700

    declutr learning: Timing, pl distribution plot, removal clean-ups, and loss objective arg.

--- declutr_learning.py
+++ declutr_learning.py
@@ -1,36 +1,44 @@
 from sequence_processor import SequenceProcessor
 from declutr_trainer import DeClutrTrainer
 from code_parser import CodeParser
-from transformers import TFAutoModel, AutoTokenizer
-from transformers import ElectraTokenizerFast, ElectraModel, ElectraConfig
+from common_funcs import find_code_df_methods
 
+import os
 import sys
-
 import ast
 
-_, sampling, encoder_model = sys.argv
+from visuals import make_histogram
+
+import time
+
+# Parse scripts and build a code dataframe from them.
+_, sampling, encoder_model, loss_objective  = sys.argv
 sampling = ast.literal_eval(sampling) / 100
-code_parser = CodeParser()
+code_parser = CodeParser(code_type='python')
 code_directory = '/Users/calvinjohn/Documents'
+start = time.time()
 code_df = code_parser.code_directory_to_df(code_directory)
+parsing_time = time.time() - start
+print(f'UPDATE: Parsing time = {parsing_time}')
 code_df = code_df.sample(frac=sampling)
 print(f'UPDATE: Final code dataframe info. ')
 code_df.info()
 
-code_df = code_parser.find_code_df_methods(code_df)
+# Plot histogram of different PL's.
+layout_args = dict(title_text="Programming Language Distribution, Train Subset", title_x=0.5)
+make_histogram(code_df, column="programming_language", layout_args=layout_args)
+code_df = find_code_df_methods(code_df)
 
-# Initialize declutr processor, model and training wrapper.
-declutr_trainer = DeClutrTrainer()
 # Change tokenizer type to tf or pytorch.
 anchors_per_doc = 1
-pad_sequences = True if encoder_model == 'transformer' else False
-seq_processor = SequenceProcessor(documents_per_batch=16, chunk_size=int(1.0e3), anchors_per_document=anchors_per_doc,
-                                  pad_sequences=pad_sequences)
 
 # Fit tokenizer in chunks on the code dataframe and then start model training.
-seq_processor.fit_tokenizer_in_chunks(code_df)
-model_id = f'{encoder_model}_metal_test'
+# Initialize declutr processor, model and training wrapper.
+max_anchor_length = 64
+sequence_processor_args = dict(loss_objective=loss_objective, max_anchor_length=max_anchor_length)
+declutr_trainer = DeClutrTrainer(sequence_processor_args=sequence_processor_args, encoder_model=encoder_model)
+model_id = "_".join([encoder_model, loss_objective, f"max_anchor_length={max_anchor_length}"])
 declutr_args = dict(encoder_model=encoder_model, model_id=model_id)
-declutr_model = declutr_trainer.build_model_from_processor(seq_processor, declutr_args=declutr_args)
+declutr_model = declutr_trainer.build_declutr_model(code_df, declutr_args=declutr_args)
 declutr_trainer.train_model(declutr_model, code_df)
 
