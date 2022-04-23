170d37d1b3937336efafc8cee8914ad7b169502a
Calvin John <calvinjohn@Calvins-MacBook-Pro.local>
2022-04-22 18:14:39 -0700

    DeclutrTrainer: directory and callback cleanups, accurate cardinality estimate, better layer naming.

--- declutr_trainer.py
+++ declutr_trainer.py
@@ -3,11 +3,12 @@ import sys
 
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.metrics import CategoricalAccuracy, TopKCategoricalAccuracy
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, CSVLogger
 from tensorboard.plugins.hparams import api as hp
 from tensorflow.data.experimental import cardinality
+from tensorflow.keras.models import save_model
+from tensorflow.keras.losses import CategoricalCrossentropy as cross_entropy
 
 from sequence_models import DeClutrContrastive, DeclutrMaskedLanguage
 from sequence_processor import SequenceProcessor
@@ -27,8 +28,12 @@ tf.executing_eagerly()
 tf.config.list_physical_devices()
 
 class DeClutrTrainer(SequenceProcessor):
-    metrics = [CategoricalAccuracy(), TopKCategoricalAccuracy(k=3)]
-    # Hyper-parameters to be optimized with tensorboard hparams.
+    # Number top categories included for top k categorical accuracy. For ex, if model predicts true
+    # class as 2nd or 3rd highest, these would both be considered correct with TOP_K=3.
+    TOP_K = 3
+    metrics = [CategoricalAccuracy(), TopKCategoricalAccuracy(k=TOP_K, name=f"top_{TOP_K}_categorical_accuracy")]
+
+    # Hyper-parameters and domains optimized with tensorboard hparams.
     EMBEDDING_DIM_DOMAIN = [10, 100]
     HP_EMBEDDING_DIM = hp.HParam('embedding_dimension', hp.Discrete(EMBEDDING_DIM_DOMAIN))
     HPARAMS = [HP_EMBEDDING_DIM]
@@ -36,7 +41,8 @@ class DeClutrTrainer(SequenceProcessor):
     DECLUR_MODEL_LOSSES = dict(declutr_contrastive=ContrastiveLoss, declutr_masked_language=MaskedMethodLoss)
 
     def __init__(self, sequence_processor_args={}, chunk_size=1000, epoch_count=3, train_split=.75,
-                 metrics=[], declutr_model="declutr_contrastive", encoder_model='lstm'):
+                 metrics=[], declutr_model="declutr_contrastive", encoder_model='lstm', save_format="tf",
+                 tensorboard_dir="tensorboard_logs"):
 
         super().__init__(**sequence_processor_args)
         declutr_model = self.loss_objective
@@ -49,7 +55,7 @@ class DeClutrTrainer(SequenceProcessor):
         self.declutr_model = declutr_model
         self.declutr_model_class = self.DECLUTR_MODEL_CLASSES[self.declutr_model]
         sequence_processor_args["loss_objective"] = self.declutr_model
-        self.loss = self.DECLUR_MODEL_LOSSES[self.declutr_model]()
+        self.loss = cross_entropy(name=self.declutr_model)
         self.chunk_size = chunk_size
         self.epoch_count = epoch_count
         self.train_split = train_split
@@ -63,6 +69,9 @@ class DeClutrTrainer(SequenceProcessor):
             print(f'WARNING: Pad sequences not set and transformer encoder is requested! Setting pad'
                   f' sequences = True. ')
             self.pad_sequences = True
+
+        self.save_format = save_format
+        self.tensorboard_dir = tensorboard_dir
 
     def save_to_model_dir(self):
         if not self.model_dir:
@@ -85,20 +94,25 @@ class DeClutrTrainer(SequenceProcessor):
         hparam_callback = hp.KerasCallback(log_dir, hparam_val_dic)
         return hparam_callback
 
-    def set_seq_processor(self, seq_processor):
-        print(f'UPDATE: setting Trainers sequence processor to {seq_processor}.')
-        self.seq_processor = seq_processor
-
     def update_declutr_encoder_args(self, declutr_args, vocab_size):
         # Increment input dimension to account for the masked token if using a MLM architecture.
         declutr_args['input_dim'] = vocab_size if self.declutr_model_class == 'declutr_contrastive' else vocab_size + 1
         declutr_args['batch_size'] = self.batch_size
         return declutr_args
 
+    def update_from_model(self, declutr_model):
+        self.model_dir = declutr_model.model_dir
+        print(f'UPDATE: Setting Trainer model directory to {self.model_dir}.')
+
+        if self.save_format == 'tf':
+            self.model_path = self.model_dir
+        else:
+            self.model_path = os.path.join(self.model_dir, "declutr_model.h5")
+
     def build_declutr_model(self, code_df, declutr_args={}):
         self.fit_tokenizer_in_chunks(code_df)
         vocab_size = self.get_vocab_size()
-        print(f'UPDATE: Vocab size for code df = {vocab_size}.')
+        print(f'\n UPDATE: Vocab size for code df = {vocab_size}.')
         declutr_args = self.update_declutr_encoder_args(declutr_args, vocab_size)
 
         if self.declutr_model == 'declutr_contrastive':
@@ -114,28 +128,26 @@ class DeClutrTrainer(SequenceProcessor):
                                                masked_token=self.MASKED_INDEX, **declutr_args)
 
         declutr.compile(optimizer=Adam(), loss=self.loss, metrics=self.metrics, run_eagerly=True)
-        print(f'UPDATE: Setting Trainer model directory to {declutr.model_dir}.')
-        self.model_dir = declutr.model_dir
+        self.update_from_model(declutr)
 
         # Save trainer to the model directory for easy coordination during later usage\loading.
         self.save_to_model_dir()
         return declutr
 
-    def get_model_callbacks(self, log_dir='logs', hparam_tuning=True, hparam_vals=None):
+    def get_model_callbacks(self, hparam_tuning=True, hparam_vals=None):
         '''
         Get keras callbacks for helping with model training and providing analysis data for it.
 
         Inputs
-        model_dir:
-        log_dir:
-        hparam_tuning:
+        hparam_tuning (bool): Whether to add HParam callback for hyperparameter tuning.
+        hparam_vals (list): List of values each hyperparam is taking.
         '''
 
         # Set CSV log directory to be chunk, epoch dependent to avoid overwriting when training over
         # different chunks and epochs.
         log_path = os.path.join(self.log_dir, 'fit_log.csv')
         csv_logger = CSVLogger(filename=log_path, append=True)
-        tensorboard = TensorBoard(log_dir=log_dir, write_images=True, update_freq='batch')
+        tensorboard = TensorBoard(log_dir=self.tensorboard_dir, write_images=True, update_freq='batch')
         checkpoint = ModelCheckpoint(filepath=os.path.join(self.model_dir, 'checkpoint'))
         callbacks = [csv_logger, tensorboard, checkpoint]
 
@@ -144,7 +156,7 @@ class DeClutrTrainer(SequenceProcessor):
                 print(f'ERROR: No hparam vals provided for hparam tuning! ')
                 sys.exit(1)
 
-            hparam_callback = self.build_hparam_callback(log_dir, hparam_vals)
+            hparam_callback = self.build_hparam_callback(self.model_dir, hparam_vals)
             callbacks.append(hparam_callback)
 
         return callbacks
@@ -168,6 +180,8 @@ class DeClutrTrainer(SequenceProcessor):
             declutr.fit(x=dataset_train, epochs=1, callbacks=callbacks, validation_data=dataset_val,
                         steps_per_epoch=train_steps, validation_steps=val_steps)
             print(f'UPDATE: Train time = {time.time() - start}.')
+            save_model(declutr, filepath=self.model_path, save_format=self.save_format)
+            print(f'UPDATE: Saved model to {self.model_dir}.')
 
     def build_fit_directory(self):
         self.log_dir = os.path.join(self.model_dir, f'chunk_{self.chunk}')
@@ -180,7 +194,7 @@ class DeClutrTrainer(SequenceProcessor):
     def get_batch_count(self, document_df):
         if self.declutr_model == 'declutr_contrastive':
             # By default, one batch of negative samples for each document as an anchor sample.
-            return len(document_df)
+            return self.cardinality_estimate
         else:
             return self.count_declutr_mmm_batches(document_df)
 
@@ -191,6 +205,7 @@ class DeClutrTrainer(SequenceProcessor):
         '''
 
         self.fit_tokenizer_in_chunks(document_df)
+
         self.chunk_count = math.ceil(len(document_df) / self.chunk_size)
 
         for chunk, chunk_df in enumerate(self.partition_df(document_df, chunk_size=self.chunk_size)):
@@ -199,10 +214,8 @@ class DeClutrTrainer(SequenceProcessor):
                       f'epoch {epoch}, chunk size = {len(chunk_df)}.')
                 self.update_indices(chunk, epoch)
                 self.build_fit_directory()
-
-                # Cardinality of -2 means that the cardinality couldn't be computed.
                 dataset = self.get_dataset(chunk_df)
-                self.batch_count = cardinality(dataset) if cardinality(dataset) != tf.data.experimental.UNKNOWN_CARDINALITY \
+                self.batch_count = cardinality(dataset) if cardinality(dataset) != tf.data.experimental.UNKNOWN_CARDINALITY\
                               else self.get_batch_count(chunk_df)
                 train_steps = int(self.batch_count * self.train_split)
                 val_steps = self.batch_count - train_steps
