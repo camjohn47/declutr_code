296b58211ba2e7e976f931bb23cea044dd5ea4a7
Calvin John <calvinscloud@icloud.com>
2022-04-23 16:43:14 -0700

    visuals: default histgoram comparison x range.

--- visuals.py
+++ visuals.py
@@ -6,6 +6,9 @@ import math
 
 import sys
 
+# Default for histogram comparison.
+xaxis_range = [0, 1000]
+
 def make_histogram(df, column, layout_args={}, save_path=None):
     fig = px.histogram(df, x=column)
     fig.update_layout(layout_args)
@@ -15,7 +18,7 @@ def make_histogram(df, column, layout_args={}, save_path=None):
         print(f'UPDATE: Saving plotly histogram to {save_path}.')
         fig.savefig(save_path)
 
-def make_3d_scatter(df, xyz_columns=[], layout_args={}, save_path=None, color_column=None):
+def make_3d_scatter(df, xyz_columns=["x", "y", "z"], layout_args={}, save_path=None, color_column=None):
     '''
     Make a 3D Plotly scatter of columns in a dataframe <df>.
     '''
@@ -38,16 +41,21 @@ def make_3d_scatter(df, xyz_columns=[], layout_args={}, save_path=None, color_co
         fig.savefig(save_path)
 
 def make_histogram_comparison(hist_vals, rows, cols, subplot_titles=[], subplot_xaxis=[], subplot_yaxis=[], layout_args={},
-                              save_path=None, histnorm=None, xbins=None):
+                              save_path=None, histnorm=None, xbins=None, xaxis_range=None):
+    '''
+    Compare histograms side-by-side in a single plot.
+    '''
+
     fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
     min_doc_size = min(hist_vals[1])
+    xaxis_range = xaxis_range if xaxis_range else xaxis_range
 
     for i in range(len(hist_vals)):
         # Shift up row and column by 1 because plotly rows\cols are 1-indexed.
         row = math.floor(i / cols) + 1
         col = (i % cols) + 1
         fig.add_trace(go.Histogram(x=hist_vals[i], histnorm=histnorm, xbins=xbins), row=row, col=col)
-        fig.update_xaxes(subplot_xaxis[i], row=row, col=col)
+        fig.update_xaxes(subplot_xaxis[i], row=row, col=col, range=xaxis_range)
         fig.update_yaxes(subplot_yaxis[i], row=row, col=col)
         fig.add_vline(min_doc_size, row=row, col=col)
 
