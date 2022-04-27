3da5ea0c2b912db1affb1c9a7857284d32ec8b21
Calvin John <calvinscloud@icloud.com>
2022-04-26 17:48:40 -0700

    Trainer: code parser attribute, building dataframe and visuals, training in one method, and TensorVisualizer callback. Simplified declutr_learning script using new Trainer.

--- declutr_learning.py
+++ declutr_learning.py
@@ -1,9 +1,7 @@
+import os
+
 from declutr_trainer import DeClutrTrainer
 from code_parser import CodeParser
-from common_funcs import find_code_df_methods, run_with_time
-
-from visuals import make_histogram
-
 import argparse
 
 #TODO: Simplify this using CustomArgParser.
@@ -31,25 +29,14 @@ def get_args():
                                 save_format=args["save_format"])
     declutr_model_args = dict(encoder_model=args["encoder_model"], model_id=args["model_id"])
     sampling = args["sampling"]
-    return code_parser_args, sequence_processor_args, declutr_trainer_args, declutr_model_args, sampling
+    declutr_trainer_args["code_parser_args"] = code_parser_args
+    return sequence_processor_args, declutr_trainer_args, declutr_model_args, sampling
 
 # Parse scripts and build a code dataframe from them.
-code_parser_args, sequence_processor_args, declutr_trainer_args, declutr_model_args, sampling = get_args()
-code_parser = CodeParser(**code_parser_args)
-code_directory = '/Users/calvinjohn/Documents'
-code_df = run_with_time(code_parser.code_directory_to_df, dict(script_directory=code_directory), "CodeParser df building")
-code_df = code_df.sample(frac=sampling)
-print(f'UPDATE: Final code dataframe info. ')
-code_df.info()
-
-# Plot histogram of different PL's.
-layout_args = dict(title_text="Programming Language Distribution, Train Subset", title_x=0.5)
-make_histogram(code_df, column="programming_language", layout_args=layout_args)
-code_df = run_with_time(find_code_df_methods, dict(code_df=code_df), "find code df methods")
+sequence_processor_args, declutr_trainer_args, declutr_model_args, sampling = get_args()
 
 # Initialize declutr processor, model and training helper.
 declutr_trainer = DeClutrTrainer(**declutr_trainer_args)
-build_model_args = dict(code_df=code_df, declutr_args=declutr_model_args)
-declutr_model = run_with_time(declutr_trainer.build_declutr_model, kwargs=build_model_args, name="build DeClutr model")
-declutr_trainer.train_model(declutr_model, code_df)
+curr_directory = os.getcwd()
+declutr_trainer.start_training_from_directory(code_directory=curr_directory, declutr_args=declutr_model_args)
 
--- declutr_trainer.py
+++ declutr_trainer.py
@@ -10,9 +10,13 @@ from tensorflow.data.experimental import cardinality
 from tensorflow.keras.models import save_model
 from tensorflow.keras.losses import CategoricalCrossentropy as cross_entropy
 
+from common_funcs import run_with_time
+from code_parser import CodeParser
 from sequence_models import DeClutrContrastive, DeclutrMaskedLanguage
 from sequence_processor import SequenceProcessor
 from loss_functions import ContrastiveLoss, MaskedMethodLoss
+from tensor_visualier import TensorVisualizer, VisualizerCallBack
+from visuals import make_histogram, process_fig
 
 from itertools import product
 
@@ -22,7 +26,6 @@ import time
 
 import math
 
-import pickle
 import dill
 
 tf.executing_eagerly()
@@ -40,10 +43,11 @@ class DeClutrTrainer(SequenceProcessor):
     HPARAMS = [HP_EMBEDDING_DIM]
     DECLUTR_MODEL_CLASSES = dict(declutr_contrastive=DeClutrContrastive, declutr_masked_language=DeclutrMaskedLanguage)
     DECLUR_MODEL_LOSSES = dict(declutr_contrastive=ContrastiveLoss, declutr_masked_language=MaskedMethodLoss)
+    code_parser_args = dict(programming_language="all")
 
-    def __init__(self, sequence_processor_args={}, chunk_size=1000, epoch_count=3, train_split=.75,
-                 metrics=[], declutr_model="declutr_contrastive", encoder_model='lstm', save_format="tf",
-                 tensorboard_dir="tensorboard_logs", save_training_data=True):
+    def __init__(self, sequence_processor_args={}, code_parser_args={}, chunk_size=1000, epoch_count=3, train_split=.75,
+                 metrics=[], declutr_model="declutr_contrastive", encoder_model='lstm', save_format="tf", model_dir="models",
+                 tensorboard_dir="tensorboard_logs", save_training_data=True, visualize_tensors=False, sampling=1):
 
         super().__init__(**sequence_processor_args)
         declutr_model = self.loss_objective
@@ -75,7 +79,52 @@ class DeClutrTrainer(SequenceProcessor):
         self.tensorboard_dir = tensorboard_dir
         self.save_training_data = save_training_data
 
+        # Whether to use TensorVisualizer for visualizing training outputs and labels.
+        self.visualize_tensors = visualize_tensors
+
+        # Parser for finding scripts and building text dataframes from which sequences are made.
+        self.model_dir = model_dir
+        self.code_parser_args.update(code_parser_args)
+        self.code_parser = CodeParser(**self.code_parser_args)
+        self.sampling = sampling
+
+    def build_code_df(self, code_directory):
+        func = self.code_parser.code_directory_to_df
+        kwargs = dict(script_directory=code_directory)
+        code_df = run_with_time(func=func, kwargs=kwargs, name="CodeParser df building")
+        code_df = code_df.sample(frac=self.sampling)
+        print(f'UPDATE: DeClutrTrainer has built code df with sampling={self.sampling}. Info shown below.')
+        code_df.info()
+        return code_df
+
+    def make_programming_language_hist(self, code_df):
+        layout_args = dict(title_text="Programming Language Distribution, Train Subset", title_x=0.5)
+        hist = make_histogram(code_df, column="programming_language", layout_args=layout_args)
+        path = os.path.join(self.model_dir, self.model_id, "programming_language_histogram.png")
+        process_fig(fig=hist, path=path)
+
+    def start_training_from_directory(self, code_directory, declutr_args={}):
+        '''
+        Parse through a code directory to extract a text dataframe. Then, build sequences from this text and train a
+        Declutr model with them.
+        '''
+
+        if "model_id" not in declutr_args:
+            print(f'ERROR: Model is must be specified in declutr_args! ')
+            sys.exit(1)
+
+        self.model_id = declutr_args["model_id"]
+        code_df = self.build_code_df(code_directory)
+        self.make_programming_language_hist(code_df)
+        declutr_model = self.build_declutr_model(code_df=code_df, declutr_args=declutr_args)
+        self.train_model(declutr_model=declutr_model, document_df=code_df)
+
     def save_to_model_dir(self):
+        '''
+        Save this trainer instance with Dill to the tensorflow model directory. That way, when loading the model later,
+        you can easily produce sequences with the same processor used for training.
+        '''
+
         if not self.model_dir:
             print(f'ERROR: Tried to save DeClutr Trainer before building model directory! ')
             sys.exit(1)
@@ -93,7 +142,6 @@ class DeClutrTrainer(SequenceProcessor):
         '''
 
         hparam_val_dic = {hparam: hparam_vals[i] for i, hparam in enumerate(self.HPARAMS)}
-        print(f'UPDATE: hparam val dic {hparam_val_dic}')
         hparam_callback = hp.KerasCallback(log_dir, hparam_val_dic)
         return hparam_callback
 
@@ -101,6 +149,7 @@ class DeClutrTrainer(SequenceProcessor):
         # Increment input dimension to account for the masked token if using a MLM architecture.
         declutr_args['input_dim'] = vocab_size if self.declutr_model_class == 'declutr_contrastive' else vocab_size + 1
         declutr_args['batch_size'] = self.batch_size
+        declutr_args["visualize_tensors"] = self.visualize_tensors
         return declutr_args
 
     def update_from_model(self, declutr_model):
@@ -118,10 +167,21 @@ class DeClutrTrainer(SequenceProcessor):
         print(f"UPDATE: Saving encoder to {self.encoder_path}.")
         save_model(encoder, filepath=self.encoder_path, save_format=self.save_format)
 
+    def build_tensor_visualizer(self, model_dir):
+        tensor_visualizer = TensorVisualizer(tf_model_dir=model_dir) if self.visualize_tensors else None
+        return tensor_visualizer
+
     def build_declutr_model(self, code_df, declutr_args={}):
+        '''
+        Build and return a Declutr model for training using code_df for vocabulary.
+
+        Inputs
+        code_df (DataFrame): A script-based dataframe supplying text for training sequences.
+        declutr_args (dict): Parameter-value mapping with configuration for building a Declutr model.
+        '''
+
         self.fit_tokenizer_in_chunks(code_df)
         vocab_size = self.get_vocab_size()
-        print(f'\n UPDATE: Vocab size for code df = {vocab_size}.')
         declutr_args = self.update_declutr_encoder_args(declutr_args, vocab_size)
 
         if self.declutr_model == 'declutr_contrastive':
@@ -133,15 +193,23 @@ class DeClutrTrainer(SequenceProcessor):
             masked_vocabulary_size = self.get_method_vocab_size()
             print(f'UPDATE: Building MML model with masked vocab size = {masked_vocabulary_size}, '
                   f'masked index = {self.MASKED_INDEX} declutr args = {declutr_args}.')
-            declutr = self.declutr_model_class(masked_vocabulary_size=masked_vocabulary_size,
-                                               masked_token=self.MASKED_INDEX, **declutr_args)
+            declutr = self.declutr_model_class(masked_vocabulary_size=masked_vocabulary_size, masked_token=self.MASKED_INDEX,
+                                               **declutr_args)
 
         declutr.compile(optimizer=Adam(), loss=self.loss, metrics=self.metrics, run_eagerly=True)
         self.update_from_model(declutr)
 
         # Save trainer to the model directory for easy coordination during later usage\loading.
         self.save_to_model_dir()
+        self.tensor_visualizer = declutr.tensor_visualizer
         return declutr
+
+    def update_callbacks_visuals(self, callbacks):
+        if self.visualize_tensors:
+            visualize_callback = VisualizerCallBack(self.tensor_visualizer)
+            callbacks.append(visualize_callback)
+
+        return callbacks
 
     def get_model_callbacks(self, hparam_tuning=True, hparam_vals=None):
         '''
@@ -168,6 +236,7 @@ class DeClutrTrainer(SequenceProcessor):
             hparam_callback = self.build_hparam_callback(self.model_dir, hparam_vals)
             callbacks.append(hparam_callback)
 
+        callbacks = self.update_callbacks_visuals(callbacks)
         return callbacks
 
     def train_over_grid_search(self, declutr, dataset_train, dataset_val, train_steps, val_steps):
@@ -184,7 +253,7 @@ class DeClutrTrainer(SequenceProcessor):
         for hparam_combo in hparam_combos:
             callbacks = self.get_model_callbacks(hparam_vals=hparam_combo)
             print(f'UPDATE: Beginning training with HParam combo {hparam_combo}. Training steps = {train_steps},'
-                  f' validation steps = {val_steps}')
+                  f' validation steps = {val_steps}, callbacks = {callbacks}')
             start = time.time()
             declutr.fit(x=dataset_train, epochs=1, callbacks=callbacks, validation_data=dataset_val,
                         steps_per_epoch=train_steps, validation_steps=val_steps)
@@ -208,7 +277,7 @@ class DeClutrTrainer(SequenceProcessor):
         else:
             return self.count_declutr_mmm_batches(document_df)
 
-    def train_model(self, declutr, document_df):
+    def train_model(self, declutr_model, document_df):
         '''
         For each epoch, create new anchor spans + positive and negative samples from the document df.
         Then use these sequences as inputs and outputs for training the declutr model.
@@ -238,5 +307,5 @@ class DeClutrTrainer(SequenceProcessor):
                 dataset_train = dataset.take(train_steps)
                 dataset_val = dataset.skip(train_steps)
                 print(f'UPDATE: Batch count = {self.batch_count}, train count = {train_steps}, val steps = {val_steps}')
-                self.train_over_grid_search(declutr, dataset_train, dataset_val, train_steps, val_steps)
+                self.train_over_grid_search(declutr_model, dataset_train, dataset_val, train_steps, val_steps)
 
