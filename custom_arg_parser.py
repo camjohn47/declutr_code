fd5f2e3b518f698f5b46a6e4f5ebe38179d288bd
Calvin John <calvinscloud@icloud.com>
2022-05-20 09:54:20 -0700

    CustomArgParser: Simpler argument declaration, kwarg parsing, bug fix in get_argument_subsets.

--- custom_arg_parser.py
+++ custom_arg_parser.py
@@ -1,16 +1,43 @@
 from argparse import ArgumentParser
-
 import sys
 
 class CustomArgParser(ArgumentParser):
     def __init__(self):
         super().__init__()
 
-    def add_arguments(self, arguments_args, arguments_kwargs):
-        args_kwargs = zip(arguments_args, arguments_kwargs)
-        for args, kw_args in args_kwargs:
+    @staticmethod
+    def join_dicts(dicts):
+        joined_dict = {}
+
+        for x in dicts:
+            joined_dict = {**x, **joined_dict}
+
+        return joined_dict
+
+    def parse_kwargs(self, argument_args):
+        kwargs = [x for x in argument_args if isinstance(x, dict)]
+        kwargs = self.join_dicts(kwargs)
+        argument_args.remove(kwargs)
+        return argument_args, kwargs
+
+    def get_arg_kwarg_pairs(self, arguments_args):
+        arg_kwarg_pairs = []
+
+        for argument_args in arguments_args:
+            argument_args, argument_kwargs = self.parse_kwargs(argument_args)
+            arg_kwarg_pair = [argument_args, argument_kwargs]
+            arg_kwarg_pairs.append(arg_kwarg_pair)
+
+        return arg_kwarg_pairs
+
+    def add_arguments(self, arguments_args):
+        argument_pairs = self.get_arg_kwarg_pairs(arguments_args)
+
+        for args, kw_args in argument_pairs:
+            print(f"UPDATE: Adding args = {args}, kwargs={kw_args}")
             self.add_argument(*args, **kw_args)
 
+    #TODO: Single list flexibility for uniform argument subsets.
     def get_argument_subsets(self, arg_name_subsets):
         self.args = vars(self.parse_args())
         arg_subsets = []
@@ -22,7 +49,8 @@ class CustomArgParser(ArgumentParser):
                       f'CustomArgParser args = {(self.args)}.')
                 sys.exit(1)
 
-            arg_subset = {arg: self.args[arg] for arg in arg_name_subset}
+            arg_count = len(arg_name_subset)
+            arg_subset = [self.args[arg] for arg in arg_name_subset] if arg_count > 1 else self.args[arg_name_subset[0]]
             arg_subsets.append(arg_subset)
 
         return arg_subsets
