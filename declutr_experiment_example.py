0b5f34e2a2a025e4a98a6f32317c9a5f266b39dd
Calvin John <calvinscloud@icloud.com>
2022-05-28 14:39:06 -0700

    Removing declutr_experiment_example. Was renamed to experiment_example.py

--- declutr_experiment_example.py
+++
@@ -1,5 +0,0 @@
-from declutr_experiment import DeclutrExperiment
-
-# Simple to run Declutr encoding model experiment.
-experiment = DeclutrExperiment(variable_arg="encoding_model", variable_domain=["rnn", "transformer"])
-experiment.run()
