c689c8ff75dd0fe18d4c4456dec2d479cf1deca2
Calvin John <calvinscloud@icloud.com>
2022-06-08 18:36:42 -0700

    ModelOrganizer: Mean-reducing metrics for each chunk.

--- experiment_example.py
+++ experiment_example.py
@@ -1,6 +1,8 @@
 from declutr_experiment import DeclutrExperiment
 
 # Simple to run Declutr encoding model experiment.
-experiment = DeclutrExperiment(variable_arg="encoder_model",
-                               variable_domain=["rnn", "transformer"])
+experiment = DeclutrExperiment(variable_arg="use_positional_encodings",
+                               variable_domain=[True, False],
+                               constant_arg_vals=dict(encoder_model="transformer_encoder", sampling=.1),
+                               add_constants_to_id=["encoder_model"])
 experiment.run()
--- model_organizer.py
+++ model_organizer.py
@@ -28,14 +28,34 @@ class ModelOrganizer():
 
     @staticmethod
     def sort_by_chunk_epoch(df):
-        df.sort_values(by=["chunk", "epoch"], inplace=True)
+        df.sort_values(by=["chunk"], inplace=True)
         columns = df.columns.tolist()
         columns.pop(-1)
         columns.insert(0, "chunk")
         df = df[columns]
         return df
 
+    @staticmethod
+    def mean_reduce_dataframe(df):
+        '''
+        Returns dataframe with same columns and one row with their means.
+        '''
+
+        col_means = [[df[col].mean() for col in df.columns]]
+        mean_df = DataFrame(col_means, columns=df.columns)
+        return mean_df
+
     def collect_results(self, model_dir):
+        '''
+        Collects and organizes performance results found in input <model_dir>.
+
+        Outputs
+        collected_results (DataFrame): Dataframe with following columns:
+
+                                       i) chunk (which document chunk of the dataset results are for),
+                                       ii-...) average performance for each considered metric.
+        '''
+
         model_subdir_pattern = os.path.join(model_dir, "*")
         model_subdirs = [path for path in glob(model_subdir_pattern) if os.path.isdir(path)]
         collected_results = DataFrame([])
@@ -47,13 +67,18 @@ class ModelOrganizer():
             if fit_log.empty:
                 continue
 
-            fit_log = self.increment_epochs(fit_log)
+            fit_log = self.mean_reduce_dataframe(fit_log)
             chunk_index = self.get_fit_log_chunk(fit_log_path)
-            fit_log["chunk"] = [chunk_index] * len(fit_log)
+            fit_log["chunk"] = [chunk_index]
             collected_results = pd.concat([collected_results, fit_log])
 
         collected_results = self.sort_by_chunk_epoch(collected_results)
         return collected_results
+
+    @staticmethod
+    def take_top_results(results_df, metric, top_k=3):
+        top_results_df = results_df.nlargest(metric, top_k)
+        return top_results_df
 
     @staticmethod
     def increment_epochs(fit_df):
